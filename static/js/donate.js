8a67dcfec57ad09f6aac2f5ba7d80e60584646cb
Colin Finck <colin.finck@rwth-aachen.de>
2018-08-19 23:41:47 +0200

    Implement the jQuery-based JavaScript for the Donation page.

--- static/js/donate.js
+++ static/js/donate.js
@@ -1,2 +1,15 @@
-/* TODO */
+$(function() {
+	$("#other_amount").keyup(function() {
+		$(this).val($(this).val().replace(/[^0-9]/g, ""));
+		if ($(this).val() != "") {
+			$("INPUT[name='amount_options']").filter(":checked").removeAttr("checked");
+			$("#amount_buttons > .active").removeClass("active");
+			$(".hidden_amounts").val($(this).val());
+		}
+	});
 
+	$("INPUT[name='amount_options']").change(function() {
+		$("#other_amount").val("");
+		$(".hidden_amounts").val($(this).val());
+	});
+});
