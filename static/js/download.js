fc4084491e909258898e74bd5c336bc4e983a85f
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2026-07-30 19:44:55 +0200

    Fix the Download button, following commit f628d738c8 (PR #142)

--- static/js/download.js
+++ static/js/download.js
@@ -1,7 +1,7 @@
 var bootcd = $("#bootcd").attr("href");
 $("#bootcd").removeAttr("href");
 $(".modalbtn").click(function(e){
+  e.preventDefault();
   $("#bootcdModal").modal("toggle");
-  url = bootcd;
-  $('.modal-footer a').attr("href", url)
+  $('.modal-footer a').attr("href", bootcd)
 });
