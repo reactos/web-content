1294dc815f412a9b81f96f2a4f41d2aef86590aa
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2026-07-30 19:12:30 +0200

    Fix the Download button following PR #142

--- static/js/download.js
+++ static/js/download.js
@@ -2,7 +2,7 @@ var bootcd = $("#bootcd").attr("href");
 var livecd = $("#livecd").attr("href");
 $("#bootcd").removeAttr("href");
 $("#livecd").removeAttr("href");
-$(".modalbtn").click(function(e){
+$(".buttons").click(function(e){
   $("#bootcdModal").modal("toggle");
   url = bootcd;
   if (e.target.textContent == "LiveCD") {
